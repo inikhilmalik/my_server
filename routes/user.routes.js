1b56ccb4f5742e5e054175a9c3380a882d09aa19
Nikhil <inikhilmalik@gmail.com>
2023-08-07 12:41:32 +0530

    m

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,64 +1,64 @@
-// const express = require("express");
-// const jwt = require("jsonwebtoken");
-// const bcrypt = require("bcrypt");
-// const { UserModel } = require("../models/user.model");
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { UserModel } = require("../models/user.model");
 
-// const userRouter = express.Router();
+const userRouter = express.Router();
 
-// userRouter.post("/signup", async (req, res) => {
-//   const { name, email, password, permissions,number,role } = req.body;
-//   try {
-//     if (!name || !email || !password) {
-//       res.status(400).send({ message: "Please enter al the feilds" });
-//     }
+userRouter.post("/signup", async (req, res) => {
+  const { name, email, password, permissions,number,role } = req.body;
+  try {
+    if (!name || !email || !password) {
+      res.status(400).send({ message: "Please enter al the feilds" });
+    }
 
-//     const userExists = await UserModel.findOne({ email });
-//     if (userExists) {
-//       res.status(400).send("User already exists")
-//     }
-//     else {
-//       bcrypt.hash(password, 1, async (err, hash) => {
-//         if (err) {
-//           res.status(400).send({ error: err.message });
-//         } else {
-//           const user = new UserModel({ ...req.body, password: hash });
-//           await user.save();
-//           res.status(200).send({ message: `New User Has Been Registered` });
-//         }
-//       });
-//     }
-//   }
-//   catch (err) {
-//     res.status(400).send({ error: err.message })
-//   }
-// })
+    const userExists = await UserModel.findOne({ email });
+    if (userExists) {
+      res.status(400).send("User already exists")
+    }
+    else {
+      bcrypt.hash(password, 1, async (err, hash) => {
+        if (err) {
+          res.status(400).send({ error: err.message });
+        } else {
+          const user = new UserModel({ ...req.body, password: hash });
+          await user.save();
+          res.status(200).send({ message: `New User Has Been Registered` });
+        }
+      });
+    }
+  }
+  catch (err) {
+    res.status(400).send({ error: err.message })
+  }
+})
 
-// userRouter.post("/login", async (req, res) => {
-//   const { email, password } = req.body;
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-//   try {
-//     const user = await UserModel.findOne({ email });
-//     if (!user) {
-//       return res.status(400).send({ message: "Invalid email, please Signup first" });
-//     }
+  try {
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(400).send({ message: "Invalid email, please Signup first" });
+    }
 
-//     const isPasswordValid = await bcrypt.compare(password, user.password);
-//     if (!isPasswordValid) {
-//       return res.status(400).send({ message: "Invalid password" });
-//     }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).send({ message: "Invalid password" });
+    }
 
-//     const token = jwt.sign({ userID: user._id }, "chatapp", { expiresIn: "12 hr" });
-//     res.status(200).send({
-//       message: "User logged in successfully",
-//       token,
-//       user
-//     });
-//   } catch (error) {
-//     res.status(400).send({
-//       message: "Something went wrong. Please try again later.",
-//     });
-//   }
-// });
+    const token = jwt.sign({ userID: user._id }, "chatapp", { expiresIn: "12 hr" });
+    res.status(200).send({
+      message: "User logged in successfully",
+      token,
+      user
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: "Something went wrong. Please try again later.",
+    });
+  }
+});
 
 // userRouter.get("/getUsers", async (req, res) => {
 //   try {
@@ -87,4 +87,4 @@
 //   }
 // })
 
-// module.exports = { userRouter }
+module.exports = { userRouter }
