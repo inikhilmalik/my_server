814bab139894dc83a46ba5d2228b592ee547052f
Nikhil Malik <inikhilmalik@gmail.com>
2023-10-23 16:11:08 +0530

    add

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -18,7 +18,8 @@ projectRouter.get("/:id",async(req,res)=>{
     const {id}=req.params
     console.log(id)
     try{
-        const data=await ProjectModal.find({_id:id});
+        const data=await ProjectModal.find({adminID:id});
+        console.log(data)
         res.send(data)
     }catch(err){
         res.send({"err":err.message})
